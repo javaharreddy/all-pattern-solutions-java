fc173ae865f685d73c6686c57bb66d0191e670d7
Sunkireddy.Javahar Reddy <89154426+javaharreddy@users.noreply.github.com>
2023-05-20 23:41:25 +0530

    Update pattern 10.java

--- pattern 10.java
+++ pattern 10.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            System.out.println("* ".repeat(i));
+        }
+        for(int i=1;i<n;i++){
+            System.out.println("* ".repeat(n-i));
+        }
+    }
+}
