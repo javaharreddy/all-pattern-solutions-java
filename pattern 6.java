be329307db9c3216c22e8c2c8a0d7116464d799e
Sunkireddy.Javahar Reddy <89154426+javaharreddy@users.noreply.github.com>
2023-05-20 23:17:20 +0530

    Create pattern 6.java

---
+++ pattern 6.java
@@ -0,0 +1,10 @@
+class Solution {
+    void printTriangle(int n) {
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
